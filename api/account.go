1fb3ed60297babd7da93bfed18eb2420bde27ce9
Divyansh12 <divag.bvp@gmail.com>
2024-01-13 23:56:12 +0530

    Added Delete Route

--- api/account.go
+++ api/account.go
@@ -87,3 +87,45 @@ func (server *Server) listAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, accounts)
 }
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+type deleteAccountResponse struct {
+	Message string     `json:"message"`
+	Account db.Account `json:"account"`
+}
+
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	var res deleteAccountResponse
+	account, err := server.store.GetAccount(ctx, req.ID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	res.Message = "Account Deleted"
+	res.Account = account
+
+	err = server.store.DeleteAccount(ctx, req.ID)
+
+	if err != nil {
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, res)
+}
