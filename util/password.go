3c115eac14693ad5359b104057e3ea5935c775c4
Divyansh12 <divag.bvp@gmail.com>
2024-01-20 18:10:56 +0530

    Added Create User API and Initial Tests

---
+++ util/password.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword returns the bcrypt hash of the password string
+func HashedPassword(password string) (string, error) {
+	hashedPasword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	return string(hashedPasword), nil
+}
+
+func CheckPassword(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
